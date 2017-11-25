792c2bfbccd8aa160b644d15624e0833a6348cd9
William Yang <willy.h.yang@gmail.com>
2017-11-24 18:39:06 -0800

    Clean README formatting, use exponential smoothing to forecast server load

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Server-Load-Balancer
 
 Made by Amarinder Chahal
+
 2017
-Small python script designed to select the optimal server for Berkeley CS61C students to SSH into.
-Server can be changed to whatever the user desires. 
-For Berkeley students, replace the LOGIN variable with your 3 letter login.
+
+Small python script designed to select the optimal server for Berkeley CS61C students to `ssh` into.
+
+Server can be changed to whatever the user desires.
+
+For Berkeley students, replace the `LOGIN` variable with your 3 letter login.
--- lb_hive.py
+++ lb_hive.py
@@ -1,3 +1,7 @@
+"""
+Finds optimal hive server to connect to
+"""
+
 import json
 import re
 import subprocess
@@ -6,22 +10,40 @@ from urllib.request import urlopen
 LOGIN = "" # Replace with your 3-letter login (xxx)
 
 # fetches JSON
-hive_page = 'https://hivemind-data.firebaseapp.com/latest.json'
-page = urlopen(hive_page)
-data = json.loads(page.read().decode())["data"]
+HIVE_PAGE = 'https://hivemind-data.firebaseapp.com/latest.json'
+PAGE = urlopen(HIVE_PAGE)
+DATA = json.loads(PAGE.read().decode())["data"]
 
-lowest_load = 1000
-use_env = 'hive1.cs'
+def ema(data, alpha=.15):
+    """
+    Returns the exponential moving average of the data list with smoothing factor alpha
+    """
+    result = 0
 
-# Go through each server, finds the hive with the lowest load
-for env in data:
-    match = re.match(r'(hive.*)',env)
-    if(match != None):
-        if(data[env] and data[env]["load_avgs"][0] < lowest_load):
-            use_env = env
-            lowest_load = data[env]["load_avgs"][0]
+    for i, datum in enumerate(data):
+        weight = alpha if i < len(data) - 1 else 1
+        result += weight * (1 - alpha) ** i * datum
 
-host = 'cs61c-%s@%s.berkeley.edu' % (LOGIN, use_env)
+    return result
 
-subprocess.call(["ssh %s" % host], shell=True)
+MIN_EMA, MIN_USERS = float('inf'), float('inf')
+USE_ENV = 'hive1.cs'
 
+# Go through each server, find active hive servers
+for env in DATA:
+    match = re.match(r'(hive.*)', env)
+
+    if match and 'users' in DATA[env]:
+        current_ema = ema(DATA[env]['load_avgs'])
+
+        if current_ema <= MIN_EMA:
+            current_users = len(DATA[env]['users'])
+
+            if current_ema < MIN_EMA or current_users < MIN_USERS:
+                USE_ENV = env
+                MIN_USERS = current_users
+                MIN_EMA = current_ema
+
+HOST = 'cs61c-%s@%s.berkeley.edu' % (LOGIN, USE_ENV)
+
+subprocess.call(["ssh %s" % HOST], shell=True)
